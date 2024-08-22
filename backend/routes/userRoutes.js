73e91d25068fcd08f6d140d83a7e4b1e08a6a05c
anantinmatrix <anantsinghtomar2017@gmail.com>
2024-08-23 00:26:32 +0530

    did some changes to node_modules

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import upload from '../multer/multer.js';
 import { v2 as cloudinary } from 'cloudinary'
